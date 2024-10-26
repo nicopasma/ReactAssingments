f67226de06c923bb1023a8cc3e28759a47faeb3c
nicopasma <n3pani00@students.oamk.fi>
2024-10-26 13:11:54 +0300

    api toimii vihdoin

--- opendataassingment/src/App.jsx
+++ opendataassingment/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Cocktail />
+  );
 }
 
-export default App
+function Cocktail() {
+  const [drinks, setDrinks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+        
+        const data = await response.json();
+        
+        setDrinks(data.drinks || []);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Drinkkilista</h1>
+      <div>
+        {drinks.map((drink) => (
+          <div key={drink.idDrink}>
+            <img src={drink.strDrinkThumb} alt={drink.strDrink}/>
+            <h2>{drink.strDrink}</h2>
+            <p>Category: {drink.strCategory}</p>
+            <p>Glass: {drink.strGlass}</p>
+            <p>Instructions: {drink.strInstructions}</p>
+            <h3>Ingredients</h3>
+            <ul>
+              {Array.from({ length: 15 }, (_, i) => i + 1)
+                .map((i) => ({
+                  ingredient: drink[`strIngredient${i}`],
+                  measure: drink[`strMeasure${i}`],
+                }))
+                .filter((item) => item.ingredient)
+                .map((item, index) => (
+                  <li key={index}>
+                    {item.measure ? `${item.measure} ` : ''}{item.ingredient}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
